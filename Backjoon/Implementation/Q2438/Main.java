0af1a6706d1ae52c627922d6c3f03b57b4d8b82a
Jay <injaesong0@gmail.com>
2023-08-10 21:02:35 +0900

    change dir name to implementation

---
+++ Backjoon/Implementation/Q2438/Main.java
@@ -0,0 +1,27 @@
+package Backjoon.Implementation.Q2438;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<n; i++) {
+
+            for (int j=0; j<=i; j++) {
+                sb.append("*");
+            }
+
+            System.out.println(sb);
+            sb.setLength(0);
+        }
+    }
+}
---
+++ Backjoon/Implementation/Q2441/Main.java
@@ -0,0 +1,30 @@
+package Backjoon.Implementation.Q2441;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        br.close();
+
+        char stars[] = new char[n];
+
+        for (int i=0; i<n; i++) {
+            stars[i] = '*';
+        }
+
+        for (int i=0; i<n; i++) {
+
+            for (int j=0; j<i; j++) {
+                stars[j] = ' ';
+            }
+
+            System.out.println(stars);
+        }
+    }
+}
