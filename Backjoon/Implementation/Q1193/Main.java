19364f612a0810886e0b4893d37b49bfb068839a
Jay <injaesong0@gmail.com>
2023-08-18 23:00:20 +0900

    implementation problem solving

---
+++ Backjoon/Implementation/Q1193/Main.java
@@ -0,0 +1,39 @@
+package Backjoon.Implementation.Q1193;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        br.close();
+
+        int sum = 2;
+        int boxCount = 0;
+        int start = 1;
+
+        while (num >= sum+boxCount) {
+            start = sum+boxCount;
+            boxCount += sum++-1;
+        }
+
+        int numerator = 1 + (num - start);
+        int denominator = sum - 1 - (num - start);
+
+        if (sum%2 == 0) {
+            int temp = numerator;
+            numerator = denominator;
+            denominator = temp;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(numerator).append("/").append(denominator);
+
+        System.out.println(sb);
+    }
+}
