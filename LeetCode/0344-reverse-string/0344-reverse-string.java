9898ab4f463ecbf3b5cca8d4cae355be2dce038b
Injae Song <110002292+IjjS@users.noreply.github.com>
2023-12-10 23:51:43 +0900

    Time: 1 ms (37.73%), Space: 51.6 MB (5.72%) - LeetHub

--- LeetCode/0344-reverse-string/0344-reverse-string.java
+++ LeetCode/0344-reverse-string/0344-reverse-string.java
@@ -4,9 +4,11 @@ class Solution {
         int end = s.length - 1;
 
         while (start < end) {
-            char temp = s[start];
-            s[start] = s[end];
-            s[end] = temp;
+            if (s[start] != s[end]) {
+                char temp = s[start];
+                s[start] = s[end];
+                s[end] = temp;
+            }
 
             start++;
             end--;
