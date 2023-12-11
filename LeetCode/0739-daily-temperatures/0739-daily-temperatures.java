de7431cebf63ce0fadfd1917806e72db2b828fad
Injae Song <110002292+IjjS@users.noreply.github.com>
2023-12-12 04:43:46 +0900

    Time: 34 ms (78.13%), Space: 55.3 MB (69.10%) - LeetHub

--- LeetCode/0739-daily-temperatures/0739-daily-temperatures.java
+++ LeetCode/0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,17 +1,12 @@
 class Solution {
-    Map<Integer, Integer> dailyT = new HashMap<>();
     Deque<Integer> stack = new ArrayDeque<>();
     
     public int[] dailyTemperatures(int[] temperatures) {
         int[] answer = new int[temperatures.length];
         
         for (int i = 0; i < temperatures.length; i++) {
-            dailyT.put(i, temperatures[i]);
-        }
-        
-        for (int i = 0; i < temperatures.length; i++) {
             if (!stack.isEmpty()) {
-                while (stack.peek() != null && dailyT.get(stack.peek()) < temperatures[i]) {
+                while (stack.peek() != null && temperatures[stack.peek()] < temperatures[i]) {
                     answer[stack.peek()] = i - stack.pop();
                 }
             }
