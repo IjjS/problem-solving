cba1b49fb4f276f9267685ce2e6a5271f87ae98f
Injae Song <110002292+IjjS@users.noreply.github.com>
2023-12-13 04:43:52 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1140 ms, Memory: 32760 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,37 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        Reader reader = new Reader();
 
-        // n, m
-        int n = Integer.parseInt(br.readLine().split(" ")[0]);
+        // n
+        int n = reader.nextInt();
+        int m = reader.nextInt();
 
         // n numbers & 구간 합 배열 생성
-        String inputs = br.readLine();
-        StringTokenizer st = new StringTokenizer(inputs);
         int[] sum = new int[n + 2];
 
         sum[0] = 0;
         sum[n] = 0;
 
         for (int i = 1; i <= n; i++) {
-            sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
+            sum[i] = sum[i - 1] + reader.nextInt();
         }
 
         // 구간 합 출력
-        String str;
-        while((str = br.readLine()) != null) {
-             st = new StringTokenizer(str);
-             int lowerBound = Integer.parseInt(st.nextToken()) - 1;
-             int upperBound = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < m; i++) {
+             int lowerBound = reader.nextInt() - 1;
+             int upperBound = reader.nextInt();
 
              System.out.println(sum[upperBound] - sum[lowerBound]);
         }
+    }
+}
 
-        br.close();
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32); //{ if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
     }
 }
