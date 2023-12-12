1768f8c18d84a4f81db8c3ee301379d64f7b59a0
Injae Song <110002292+IjjS@users.noreply.github.com>
2023-12-13 04:38:19 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1484 ms, Memory: 59348 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -8,14 +8,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         // n, m
-        String counts = br.readLine();
-        StringTokenizer st = new StringTokenizer(counts);
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine().split(" ")[0]);
 
         // n numbers & 구간 합 배열 생성
         String inputs = br.readLine();
-        st = new StringTokenizer(inputs);
+        StringTokenizer st = new StringTokenizer(inputs);
         int[] sum = new int[n + 2];
 
         sum[0] = 0;
@@ -26,9 +23,9 @@ public class Main {
         }
 
         // 구간 합 출력
-        for (int i = 1; i <= m; i++) {
-             String bounds = br.readLine();
-             st = new StringTokenizer(bounds);
+        String str;
+        while((str = br.readLine()) != null) {
+             st = new StringTokenizer(str);
              int lowerBound = Integer.parseInt(st.nextToken()) - 1;
              int upperBound = Integer.parseInt(st.nextToken());
 
