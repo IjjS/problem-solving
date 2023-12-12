521caafef5e42662bebd04725725651dbe977461
Injae Song <110002292+IjjS@users.noreply.github.com>
2023-12-13 03:23:58 +0900

    Time: 228 ms (63.42%), Space: 40.9 MB (17.07%) - LeetHub

--- LeetCode/0819-most-common-word/0819-most-common-word.kt
+++ LeetCode/0819-most-common-word/0819-most-common-word.kt
@@ -2,9 +2,10 @@ class Solution {
     fun mostCommonWord(paragraph: String, banned: Array<String>): String? {
         val map: MutableMap<String, Int> = mutableMapOf()
         val regex = Regex("\\W+")
-        val words = paragraph.replace(regex, " ").toLowerCase().trim().split(" ")
+        val words = paragraph.lowercase().split(regex)
         
         for (word in words) {
+            if (word.isBlank()) continue;
             if (!banned.contains(word)) {
                 map[word] = map.getOrDefault(word, 0) + 1
             }
