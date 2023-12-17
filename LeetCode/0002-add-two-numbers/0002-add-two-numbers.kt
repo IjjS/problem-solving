a6b1c59a9f0c2e1b98df070a9d2e3631fd32ff41
Injae Song <110002292+IjjS@users.noreply.github.com>
2023-12-17 15:22:13 +0900

    Time: 269 ms (5.05%), Space: 41.3 MB (38.08%) - LeetHub

---
+++ LeetCode/0002-add-two-numbers/0002-add-two-numbers.kt
@@ -0,0 +1,48 @@
+class Solution {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        val l1Num = toBigInteger(reverse(l1))
+        val l2Num = toBigInteger(reverse(l2))
+        val result = l1Num.add(l2Num)
+        
+        return toReversedList(result)
+    }
+    
+    fun reverse(head: ListNode?): ListNode? {
+        var node = head
+        var rev: ListNode? = null
+        
+        while (node != null) {
+            val next = node.next
+            node.next = rev
+            rev = node
+            node = next
+        }
+        
+        return rev
+    }
+    
+    fun toBigInteger(head: ListNode?): BigInteger {
+        var node = head
+        var str = ""
+        
+        while (node != null) {
+            str += node.`val`
+            node = node.next
+        }
+        
+        return BigInteger(str)
+    }
+    
+    fun toReversedList(value: BigInteger): ListNode? {
+        var prev: ListNode? = null
+        var node: ListNode? = null
+        
+        for (c in "$value".toCharArray()) {
+            node = ListNode(Character.getNumericValue(c))
+            node.next = prev
+            prev = node
+        }
+        
+        return node
+    }
+}
