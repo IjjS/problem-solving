d29f8cacc369883ea19a36bc9c0e0873053f456e
Injae Song <110002292+IjjS@users.noreply.github.com>
2024-01-21 20:52:55 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 45532 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+      Queue<Integer> queue = new LinkedList<>();
+      
+      for (int i = 0; i < n; i++) queue.add(i + 1);
+      
+      while (queue.size() > 1) {
+          queue.poll();
+          queue.add(queue.poll());
+      }
+      
+      System.out.println(queue.peek());
+    }
+}
