23b90e281bcdcde19e28d216ab8910d6424d4c38
Injae Song <110002292+IjjS@users.noreply.github.com>
2024-04-11 18:35:21 +0900

    Time: 16 ms (88.06%), Space: 94.6 MB (27.72%) - LeetHub

--- LeetCode/0077-combinations/0077-combinations.java
+++ LeetCode/0077-combinations/0077-combinations.java
@@ -15,26 +15,9 @@ class Solution {
         }
         
         for (int i = index; i <= n; i++) {
-            if (!combinations.contains(i)) {
-                combinations.add(i);
-                dfs(n, i + 1, k, combinations, results);
-                combinations.remove(combinations.size() - 1);
-            }
+            combinations.add(i);
+            dfs(n, i + 1, k, combinations, results);
+            combinations.removeLast();
         }
     }
 }
-
-/* n = 4, k = 2
-1, 2, 3, 4
--> 1 => 12, 13, 14
--> 2 => 23, 24
--> 3 => 34
--> 4 => X
-
-size == k ? terminate
-
-data = nums left from tha all num set in ascending order
-order = smallest i -> greater
-condition = size == k
-collections = new int list
-*/ 
